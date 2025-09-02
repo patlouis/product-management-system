4dfceba2a6e77c62154de2b5a12df430c4158532
pat <patricklouis.rivera.cics@ust.edu.ph>
2025-09-02 22:22:07 +0800

    initialize database connection pool

--- server/server.js
+++ server/server.js
@@ -1,8 +1,10 @@
 import express from "express";
+import dotenv from "dotenv";
 import cors from "cors";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+dotenv.config();
 
 app.use(cors());
 app.use(express.json());
