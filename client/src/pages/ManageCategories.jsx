cede6e152c0bbc9b10c59da41e26feef6d4be106
pat <patricklouis.rivera.cics@ust.edu.ph>
2025-09-02 22:05:40 +0800

    feat: add manage products/categories in client

---
+++ client/src/pages/ManageCategories.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function ManageCategories() {
+  return (
+    <div>
+      <h1>Manage Categories</h1>
+      <p>This is the Manage Categories page.</p>
+    </div>
+  );
+}
+
+export default ManageCategories;
---
+++ client/src/pages/ManageProducts.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function ManageProducts() {
+  return (
+    <div>
+      <h1>Manage Products</h1>
+      <p>This is the Manage Products page.</p>
+    </div>
+  );
+}
+
+export default ManageProducts;
